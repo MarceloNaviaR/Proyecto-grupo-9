cbd98318cbcbb3bd79ca0e80c93f0b77a94419c6
Diego Trigo <trigobol23@gmail.com>
2024-10-24 00:17:06 -0400

    correcion de implementacion

--- src/ingresos.js
+++ src/ingresos.js
@@ -1,10 +1,13 @@
+import Ingreso from './ingreso.js';
+
 class Ingresos {
   constructor() {
     this.ingresos = [];
   }
 
   registrarIngreso(fecha, monto, descripcion) {
-    this.ingresos.push({ fecha, monto, descripcion });
+    const nuevoIngreso = new Ingreso(fecha, monto, descripcion); // Aquí debería usarse la clase Ingreso
+    this.ingresos.push(nuevoIngreso);
   }
 
   obtenerIngresos() {
