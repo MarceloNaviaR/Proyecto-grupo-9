6d5e3e3a3fff0101737c0b6e352cf0d13605ed40
MarceloNaviaR <marcelo.navia@gmail.com>
2024-10-27 15:24:04 -0400

    Actualizar

---
+++ src/historialingresos.js
@@ -0,0 +1,22 @@
+class Ingresos {
+  constructor(ingresos) {
+    this.ingresos = ingresos;
+  }
+
+  obtenerIngresosOrdenadosPorFecha() {
+    return this.ingresos.obtenerIngresos().sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+  }
+
+  filtrarIngresosPorMontoMinimo(montoMinimo) {
+    return this.ingresos.obtenerIngresos().filter(ingreso => ingreso.monto >= montoMinimo);
+  }
+
+  filtrarIngresosPorRangoFecha(fechaInicio, fechaFin) {
+    return this.ingresos.obtenerIngresos().filter(ingreso => {
+      const fechaIngreso = new Date(ingreso.fecha);
+      return fechaIngreso >= new Date(fechaInicio) && fechaIngreso <= new Date(fechaFin);
+    });
+  }
+}
+
+export default Ingresos;
