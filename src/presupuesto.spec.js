5b6d18b7eefce0f5abdbe837df1c2ee411c5737e
Vivian Revollo <revollovivian@gmail.com>
2024-11-21 14:36:08 -0330

    Presipuesto Refactorizado

--- src/presupuesto.spec.js
+++ src/presupuesto.spec.js
@@ -1,9 +1,10 @@
 import Presupuesto from "./presupuestos.js";
 
-describe("Presupuesto", () => {
+describe("debería permitir definir un monto de presupuesto", () => {
   it("Deberia registrar el presupuesto", () => {
     const presupuestito = new Presupuesto
     presupuestito.agregarMonto(90)
     expect(presupuestito.mostrarMonto()).toEqual('Monto: 90');
   });
+
 });
